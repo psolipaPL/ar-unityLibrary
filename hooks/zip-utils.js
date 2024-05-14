9e8dfa974105279915fec99310f747ba618a39ef
ZZaRpp <neozarp@gmail.com>
2024-05-14 10:20:03 +0200

    Update zip-utils.js

--- hooks/zip-utils.js
+++ hooks/zip-utils.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const zlib = require('zlib');
 
-const fileContents = fs.createReadStream('/unityLibrary_small.zip');
+
+const fileContents = fs.createReadStream('./unityLibrary_small.zip');
 const writeStream = fs.createWriteStream('/unityLibrary');
 const unzip = zlib.createGunzip();
 
