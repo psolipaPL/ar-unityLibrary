45b113a467cc7c36800a7427aa5f38b7f70038b9
ZZaRpp <neozarp@gmail.com>
2024-05-14 17:17:49 +0200

    Update build-changer.js

--- hooks/build-changer.js
+++ hooks/build-changer.js
@@ -6,23 +6,25 @@ module.exports = function (context) {
     //const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
     
     utils.logAppFolders(context.opts.projectRoot);
-    
+    utils.logAppFolders(context.opts.projectRoot + "/www");
+    utils.logAppFolders(context.opts.projectRoot + "/res");
+    utils.logAppFolders(context.opts.projectRoot + "/hooks");
     utils.logAppFolders(context.opts.projectRoot + confs.androidAppPath);
     utils.logAppFolders(context.opts.projectRoot + confs.androidMainPath);
     utils.logAppFolders(context.opts.projectRoot + confs.androidPath);
-    utils.changeConfigXML();
-    //utils.generateUnityLibrary();
-
-    utils.unzipUnityLibrary();
     
-    utils.changeSettingsGradle();
-    utils.changeProjectProperties();
-    utils.changeGradleProperties();
-    utils.changeAndroidBuildGradle();
-    utils.changeAppBuildGradle();
+    //utils.changeConfigXML();
+    //utils.generateUnityLibrary();
+    //utils.unzipUnityLibrary();
+    
+    //utils.changeSettingsGradle();
+    //utils.changeProjectProperties();
+    //utils.changeGradleProperties();
+    //utils.changeAndroidBuildGradle();
+    //utils.changeAppBuildGradle();
 
     utils.logAppFolders(context.opts.projectRoot + confs.androidRootPath);
-    utils.logFile(context.opts.projectRoot + confs.androidRootPath + "/settings.gradle");
+    //utils.logFile(context.opts.projectRoot + confs.androidRootPath + "/settings.gradle");
     
 
 }
